56233e8afa326b44b3ca7f022147dba664427433
Mike Valeriano <mkvlrn@gmail.com>
2024-06-14 09:56:44 -0300

    chore: bump deps

--- src/math.spec.ts
+++ src/math.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from 'vitest';
 import { add } from '~/math.ts';
 
-test('add', () => {
+test(`add`, () => {
   const twoPlusTwo = add(2, 2);
   expect(twoPlusTwo).toBe(4);
 });
