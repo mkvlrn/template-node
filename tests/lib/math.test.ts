61ee51b5af5b11097fd7785c22a73050f99614fb
Mike Valeriano <mkvlrn@gmail.com>
2024-06-20 20:39:45 -0300

    chore: bump deps

--- tests/lib/math.test.ts
+++ tests/lib/math.test.ts
@@ -2,8 +2,8 @@ import { describe, expect, test } from 'vitest';
 import { divide, multiply } from '~/lib/advanced-math.ts';
 import { add, subtract } from '~/lib/basic-math.ts';
 
-describe(`basic math`, () => {
-  test(`add`, () => {
+describe('basic math', () => {
+  test('add', () => {
     const twoPlusTwo = add(2, 2);
     const twoPlusThree = add(2, 3);
 
@@ -11,7 +11,7 @@ describe(`basic math`, () => {
     expect(twoPlusThree).not.toBe(4);
   });
 
-  test(`subtract`, () => {
+  test('subtract', () => {
     const twoMinusTwo = subtract(2, 2);
     const twoMinusThree = subtract(2, 3);
 
@@ -20,8 +20,8 @@ describe(`basic math`, () => {
   });
 });
 
-describe(`advanced math`, () => {
-  test(`multiply`, () => {
+describe('advanced math', () => {
+  test('multiply', () => {
     const twoTimesTwo = multiply(2, 2);
     const twoTimesThree = multiply(2, 3);
 
@@ -29,13 +29,13 @@ describe(`advanced math`, () => {
     expect(twoTimesThree).not.toBe(4);
   });
 
-  test(`divide`, () => {
+  test('divide', () => {
     const twoDividedByTwo = divide(2, 2);
     const twoDividedByThree = divide(2, 3);
     const divideByZero = () => divide(10, 0);
 
     expect(twoDividedByTwo).toBe(1);
     expect(twoDividedByThree).not.toBe(1);
-    expect(divideByZero).toThrow(`Division by zero`);
+    expect(divideByZero).toThrow('Division by zero');
   });
 });
