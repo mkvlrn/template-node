5291831ad9f3494e713710f66cea646315713f5e
Mike Valeriano <mkvlrn@gmail.com>
2024-07-30 23:12:48 -0300

    chore: bump deps, refactor configs

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,12 @@ const twoPlusTwo = add(2, 2);
 
 // eslint-disable-next-line no-console
 console.log(twoPlusTwo);
+
+const object = {
+  a: 1,
+};
+
+Reflect.set(object, "b", 2);
+
+// eslint-disable-next-line no-console
+console.log(object);
