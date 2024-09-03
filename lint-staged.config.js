aed262dae0daf77f95671929cfb34a94dbbff9aa
Mike Valeriano <mkvlrn@gmail.com>
2024-09-03 15:10:13 -0300

    ci: only lint and format on commit, full checks on push

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,4 @@
-// this config is used on the pre-commit hook
-
 export default {
-  "*.(ts|tsx)": [() => "tsc --no-emit", "eslint --fix", "prettier --write"],
+  "*.(ts|tsx)": ["eslint --fix", "prettier --write"],
   "*.(json)": ["prettier --write"],
 };
