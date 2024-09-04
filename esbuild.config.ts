6d77df2c5af9ee7165712c43784eb0fd9e9c4b0d
Mike Valeriano <mkvlrn@gmail.com>
2024-09-04 15:40:17 -0300

    chore: bump deps

--- esbuild.config.ts
+++ esbuild.config.ts
@@ -1,20 +1,22 @@
 import { build } from "esbuild";
 import { glob } from "glob";
 import { replaceTscAliasPaths } from "tsc-alias";
+import { compilerOptions } from "./tsconfig.json";
 
 const entryPoints = await glob("src/**/*.ts");
+const { outDir } = compilerOptions;
 
 // nothing special
 await build({
   entryPoints,
-  outdir: "dist",
+  outdir: outDir,
   bundle: false,
   platform: "node",
   target: "node22",
   sourcemap: false,
   minify: false,
-  tsconfig: "tsconfig.json",
+  tsconfig: "./tsconfig.json",
 });
 
 // replace aliases with actual, relative paths
-await replaceTscAliasPaths();
+await replaceTscAliasPaths({ outDir });
