96b8844056cb011df867bca4d03a1249a6eaf6b4
Mike Valeriano <mkvlrn@proton.me>
2025-01-26 02:08:07 -0300

    chore: tidy

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,4 @@ const twoPlusTwo = add(2, 2);
 
 // biome-ignore lint/suspicious/noConsole: just a simple example
 // biome-ignore lint/suspicious/noConsoleLog: just a simple example
-console.log(twoPlusTwo);
+console.log(`2 + 2 = ${twoPlusTwo}`);
