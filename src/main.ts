2c4ca5830c937b9608db71a4577cd5d02456d7df
Mike Valeriano <mkvlrn@proton.me>
2025-05-12 12:56:17 -0300

    chore: update

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
 import { divide } from "~/lib/advanced-math.js";
 import { add } from "~/lib/basic-math.js";
 
-function main(): void {
-  const sum = add(1, 2);
-  console.log(`1 + 2 = ${sum}`);
+const sum = add(1, 2);
+console.log(`1 + 2 = ${sum}`);
 
-  const div = divide(4, 0);
-  if (div.ok) {
-    console.log(`4 / 2 = ${div.value}`);
-  } else {
-    console.error(div.error.message);
-  }
+const div = divide(4, 0);
+if (div.ok) {
+  console.log(`4 / 2 = ${div.value}`);
+} else {
+  console.error(div.error.message);
 }
-
-main();
