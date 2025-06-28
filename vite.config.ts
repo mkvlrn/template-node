49667f27f133aef11730a7ff62fd1d64e6e37909
Mike Valeriano <mkvlrn@proton.me>
2025-06-27 22:04:07 -0300

    chore: bump deps, consolidate vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,53 @@
-import { defineConfig } from "vite";
-import { getBuild } from "./.vite/build.ts";
-import { getOptimize } from "./.vite/optimize.ts";
-import { getPlugins } from "./.vite/plugins.ts";
-import { getTest } from "./.vite/test.ts";
+import nodeExternals from "rollup-plugin-node-externals";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig, type ViteUserConfig } from "vitest/config";
 
-const entryPoint = "src/main.ts";
+// application entry point
+const entry = "./src/main.ts";
 // dependencies to be internalized/bundled
 const internalPackages: string[] = ["@mkvlrn/result"];
 
-export default defineConfig({
-  build: getBuild(entryPoint),
-  plugins: getPlugins(internalPackages),
-  optimizeDeps: getOptimize(),
-  test: getTest(),
-});
+const config: ViteUserConfig = {};
+
+// esm, bundle, build settings
+config.build = {
+  target: "esnext",
+  lib: {
+    entry,
+    formats: ["es"],
+  },
+  sourcemap: true,
+  outDir: "./build",
+  emptyOutDir: true,
+};
+
+// plugins
+config.plugins = [
+  // externalize node builtins
+  nodeExternals({
+    exclude: internalPackages,
+  }),
+  // resolve tsconfig path aliases
+  tsconfigPaths(),
+];
+
+// vitest settings
+config.test = {
+  include: ["./src/**/*.test.{ts,tsx}"],
+  reporters: ["verbose"],
+  watch: false,
+  coverage: {
+    all: true,
+    clean: true,
+    cleanOnRerun: true,
+    include: ["./src"],
+    exclude: ["./src/**/*.test.{ts,tsx}", "./src/main.{ts,tsx}"],
+  },
+  // biome-ignore lint/style/useNamingConvention: needed for vitest
+  env: { NODE_ENV: "test" },
+  environment: "node",
+  passWithNoTests: true,
+  setupFiles: [],
+};
+
+export default defineConfig(config);
