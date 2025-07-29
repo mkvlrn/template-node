ee9bb6df7a017ea7dd56a16bee75455a881dc156
Mike Valeriano <mkvlrn@proton.me>
2025-07-29 06:52:59 -0300

    chore: add declaration emit and coverage package

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,14 @@
 import { divide } from "#/math/advanced-math";
 import { add } from "#/math/basic-math";
 
-function main() {
-  const sum = add(1, 2);
-  console.log(`1 + 2 = ${sum}`);
+const sum = add(1, 2);
+console.log(`1 + 2 = ${sum}`);
 
-  const by = 0;
-  const div = divide(4, by);
-  if (div.error) {
-    throw div.error;
-  }
-
-  console.log(`4 / ${by} = ${div.value}`);
+const by = 0;
+const div = divide(4, by);
+if (div.error) {
+  console.error(div.error.message);
+  process.exit(1);
 }
 
-main();
+console.log(`4 / ${by} = ${div.value}`);
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
+import { existsSync, globSync } from "node:fs";
 import nodeExternals from "rollup-plugin-node-externals";
+import dts from "vite-plugin-dts";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 // application entry point
-const entry = "./src/main.ts";
+const entry = globSync("./src/**/*.ts").filter((f) => !f.endsWith("test.ts"));
+
+// emits declarations only if there is no src/main.ts file
+const dtsPlugin = existsSync("./src/main.ts") ? null : dts({ include: entry, logLevel: "error" });
 
 export default defineConfig({
   plugins: [
@@ -11,18 +16,20 @@ export default defineConfig({
     nodeExternals(),
     // resolve tsconfig path aliases
     tsconfigPaths(),
-  ],
+    // declarations (if lib)
+    dtsPlugin,
+  ].filter(Boolean),
 
   build: {
     target: "esnext",
     lib: {
       entry,
       formats: ["es"],
-      fileName: "bundle",
     },
     sourcemap: true,
     outDir: "./build",
     emptyOutDir: true,
+    rollupOptions: { output: { preserveModules: true } },
   },
 
   test: {
@@ -33,8 +40,8 @@ export default defineConfig({
       all: true,
       clean: true,
       cleanOnRerun: true,
-      include: ["./src"],
-      exclude: ["./src/**/*.test.{ts,tsx}", "./src/main.{ts,tsx}"],
+      include: ["src"],
+      exclude: ["**/*.test.{ts,tsx}", "**/*main.ts"],
     },
     // biome-ignore lint/style/useNamingConvention: needed for vitest
     env: { NODE_ENV: "test" },
