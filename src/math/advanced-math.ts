54e77fa156277f34963066d4e29cd71380d8e617
Mike Valeriano <186238078+mkvlrn@users.noreply.github.com>
2025-08-29 08:58:42 -0300

    chore: bump deps

--- src/math/advanced-math.ts
+++ src/math/advanced-math.ts
@@ -4,7 +4,7 @@ export function multiply(a: number, b: number): number {
   return a * b;
 }
 
-export function divide(a: number, b: number): Result<number, Error> {
+export function divide(a: number, b: number): Result<number> {
   if (b === 0) {
     return R.error(new Error("Division by zero is not allowed"));
   }
