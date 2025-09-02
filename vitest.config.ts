d2f28eb8bece61cedcf72acd3a25245008ffa39d
Mike Valeriano <186238078+mkvlrn@users.noreply.github.com>
2025-09-02 06:25:00 -0300

    chore: use tsc instead of vite for building

---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    include: ["./src/**/*.test.{ts,tsx}"],
+    reporters: ["verbose"],
+    watch: false,
+    coverage: {
+      all: true,
+      clean: true,
+      cleanOnRerun: true,
+      include: ["src"],
+      exclude: ["**/*.test.{ts,tsx}", "**/*main.ts"],
+    },
+    // biome-ignore lint/style/useNamingConvention: needed for vitest
+    env: { NODE_ENV: "test" },
+    environment: "node",
+    passWithNoTests: true,
+    setupFiles: [],
+  },
+});
