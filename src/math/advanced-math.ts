18f7f5bea2eb6105bdac3d6021fc0a90c4c1fa2d
Mike Valeriano <186238078+mkvlrn@users.noreply.github.com>
2025-09-03 06:01:10 -0300

    fix: result typing

--- src/math/advanced-math.ts
+++ src/math/advanced-math.ts
@@ -4,7 +4,7 @@ export function multiply(a: number, b: number): number {
   return a * b;
 }
 
-export function divide(a: number, b: number): Result<number> {
+export function divide(a: number, b: number): Result<number, Error> {
   if (b === 0) {
     return R.error(new Error("Division by zero is not allowed"));
   }
