abc3ad163873d5a53894793b88bef0912a511e11
Mike Valeriano <mkvlrn@gmail.com>
2025-09-18 18:27:40 -0300

    chore: bump deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,21 +1,29 @@
+import process from "node:process";
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({
-  test: {
-    include: ["./src/**/*.test.{ts,tsx}"],
-    reporters: ["verbose"],
-    watch: false,
-    coverage: {
-      all: true,
-      clean: true,
-      cleanOnRerun: true,
-      include: ["src"],
-      exclude: ["**/*.test.{ts,tsx}", "**/*main.ts"],
+export default defineConfig(() => {
+  const withIntegration = process.env["INTEGRATION"] === "true";
+  const testFiles = withIntegration
+    ? ["./src/**/*.{test,spec}.{ts,tsx}"]
+    : ["./src/**/*.test.{ts,tsx}"];
+
+  return {
+    test: {
+      include: testFiles,
+      reporters: ["verbose"],
+      watch: false,
+      coverage: {
+        all: true,
+        clean: true,
+        cleanOnRerun: true,
+        include: ["src"],
+        exclude: ["**/*.{test,spec}.{ts,tsx}", "**/*main.ts"],
+      },
+      // biome-ignore lint/style/useNamingConvention: needed for vitest
+      env: { NODE_ENV: "test" },
+      environment: "node",
+      passWithNoTests: true,
+      setupFiles: [],
     },
-    // biome-ignore lint/style/useNamingConvention: needed for vitest
-    env: { NODE_ENV: "test" },
-    environment: "node",
-    passWithNoTests: true,
-    setupFiles: [],
-  },
+  };
 });
