5ef4c8d28a2b934b48577ffc7249f3bf452e0632
Mike Valeriano <mkvlrn@gmail.com>
2025-11-07 09:12:07 -0300

    chore: bump deps

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: "./src/main.ts",
+  fixedExtension: false,
   outDir: "./build",
   unbundle: true,
   sourcemap: false,
