7fe1dc3ef3504a5b63c2f261fde32466e01d16d5
mkvlrn <dev@mkvlrn.cc>
2026-08-25 02:23:29 -0300

    chore: bump deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import baseConfig from "@mkvlrn/config/vitest";
+import baseConfig from "@mkvlrn/config/vitest" with { type: "json" };
 import { defineConfig, mergeConfig } from "vitest/config";
 
 export default mergeConfig(baseConfig, defineConfig({}));
